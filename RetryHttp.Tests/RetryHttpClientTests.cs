f8592ef238a37aefc0727e6813e730ca034de907
ben-dean <ben@bdean.org>
2019-06-15 16:05:40 +0100

    Creating repo
    First push with broken code.
    
    Setting up My JSON server end point.

---
+++ RetryHttp.Tests/RetryHttpClientTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace RetryHttp.Tests
+{
+    public class RetryHttpClientTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var target = new RetryHttpClient();
+
+            var response = await target.Get
+        }
+    }
+}
